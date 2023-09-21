20fc0253bde2f24a5f852b5f23ee28deb852c3a7
servidor <bjimenez@tecnologicoargos.edu.ec>
2023-09-21 16:35:04 -0500

    prueba para servidor

---
+++ Index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>PRUEBA SISTEMAS</title>
+  <style>
+
+  </style>
+</head>
+<body>
+  <H1>Bienvenido</H1>
+<button type="reset" onclick="location.href='https://app.powerbi.com/view?r=eyJrIjoiMTJlMDNlZDMtODc3ZC00ZDBmLWEyY2MtZDFiNDMyOWZhN2I4IiwidCI6IjI1MGY3NmU3LTYxMDUtNDJlMy04MmQwLWJlN2M0NjBhZWE1OSIsImMiOjR9'">
+prueba
+</button>
+</body>
